a5b30acb1f1c301b14898ba4e8952da3cc999b6d
Rookit15 <attehiltunen@hotmail.com>
2023-08-28 23:42:07 +0300

    update with forecast if possible
    Forecast from weather-js is only available for 5 days and not on weekends so if the forecast is available it will show next days forecast

--- commands/fun/weather.js
+++ commands/fun/weather.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const weather = require('weather-js');
+const day = new Date().getDay();
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,33 +9,67 @@ module.exports = {
         .addStringOption(option => option.setName('location').setDescription('place to check the weather from').setRequired(true)),
     async execute(interaction) {
         const { options } = interaction;
-        const location = options.getString('location')
+        const location = options.getString('location');
+
         await interaction.reply(`Let me Google that for you :)`);
         await weather.find({ search: `${location}`, degreeType: 'C' }, async function (err, result) {
             if (err) {
                 console.log(err);
+                interaction.editReply('There seems to be some error try again later :)');
             } else {
                 if (result.length == 0) {
                     return interaction.editReply({ content: 'I could not find weather for this place :(' });
 
                 } else {
-                    const temp = result[0].current.temperature;
-                    const type = result[0].current.skytext;
-                    const name = result[0].location.name;
-                    const tempFeel = result[0].current.feelslike;
-                    const wind = result[0].current.windspeed;
+                    let embed = MakeEmbed(result);
+                    if (day > 0 && day < 6) {
+                        embed = forecast(embed, result, day)
+
+                        interaction.editReply({ content: ``, embeds: [embed] });
+                    } else {
+                        interaction.editReply({ content: ``, embeds: [embed] });
+                    }
 
 
-                    const embed = new EmbedBuilder()
-                        .setColor('Green')
-                        .setTitle(`Current weather of ${name}`)
-                        .addFields({ name: 'Temperature', value: `${temp} °C` })
-                        .addFields({ name: 'Weather', value: `${type}` })
-                        .addFields({ name: 'Temperature feels like', value: `${tempFeel} °C` })
-                        .addFields({ name: 'Wind', value: `${wind}` })
-                    interaction.editReply({ content: ``, embeds: [embed] });
+
                 }
             }
         })
     },
 };
+
+
+function MakeEmbed(result) {
+
+    const temp = result[0].current.temperature;
+    const type = result[0].current.skytext;
+    const name = result[0].location.name;
+    const tempFeel = result[0].current.feelslike;
+    const wind = result[0].current.windspeed;
+
+
+
+    const embed = new EmbedBuilder()
+        .setColor('Green')
+        .setTitle(`Current weather of ${name}`)
+        .addFields({ name: 'Temperature', value: `${temp} °C` })
+        .addFields({ name: 'Weather', value: `${type}` })
+        .addFields({ name: 'Temperature feels like', value: `${tempFeel} °C` })
+        .addFields({ name: 'Wind', value: `${wind}` })
+
+    return embed;
+}
+function forecast(embed, result, day) {
+
+    const forecast = result[0].forecast[day].skytextday;
+    const tempHigh = result[0].forecast[day].high;
+    const tempLow = result[0].forecast[day].low;
+    embed
+        .setColor('Blue')
+        .setTitle('Tomorrow\'s forecast')
+        .addFields({ name: 'Tomorrow\'s weather', value: `${forecast}` })
+        .addFields({ name: 'Tomorrow\'s high', value: `${tempHigh} °C` })
+        .addFields({ name: 'Tomorrow\'s low', value: `${tempLow} °C` });
+
+    return embed
+}
